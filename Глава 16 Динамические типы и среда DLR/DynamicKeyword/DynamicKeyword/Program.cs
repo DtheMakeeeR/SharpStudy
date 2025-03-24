40838d5d9b917d1969bcc2b376a5eb3bbaf5f0c0
DtheMakeeeR <danilgol45@mail.ru>
2025-03-24 12:21:09 +0300

    что-то там

---
+++ Глава 16 Динамические типы и среда DLR/DynamicKeyword/DynamicKeyword/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicKeyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //PrintThreeStnngs();
+            ChangeDynamicDataType();
+        }
+        static void PrintThreeStnngs()
+        {
+            var s1 = "word1";
+            object s2 = "word2";
+            dynamic s3 = "word3";
+            Console.WriteLine("s1 is of Type: {0}", s1.GetType());
+            Console.WriteLine("s2 is of Type: {0}", s2.GetType());
+            Console.WriteLine("s3 is of Type: {0}", s3.GetType());
+        }
+        static void ChangeDynamicDataType()
+        {
+            // Объявить одиночный динамический элемент данных по имени t.
+            dynamic t = "Hello!";
+            Console.WriteLine("t is of type: {0}", t.GetType());
+            t = false;
+            Console.WriteLine("t is of type: {0}", t.GetType());
+            t = new List<int>();
+            Console.WriteLine("t is of type: {0}", t.GetType());
+        }
+        //до исполнения нет проверки на наличие членов
+        static void InvokeMembersOnDynamicData()
+        {
+            dynamic textDatal = "Hello";
+            Console.WriteLine(textDatal.ToUpper());
+            // Здесь можно было бы ожидать ошибки на этапе компиляции!
+            // Однако все компилируется нормально.
+            Console.WriteLine(textDatal.toupper());
+            Console.WriteLine(textDatal.Foo(10, "ее", DateTime.Now));
+        }
+    }
+}
